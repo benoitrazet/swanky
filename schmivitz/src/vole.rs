ee2eac5bc9ec615e92a816d164a714c4c1a45f53
Benoit Razet <benoit.razet@galois.com>
2024-07-08 13:25:03 -0400

    schmivitz: Add interface.rs file connecting VOLE with traverser

--- schmivitz/src/vole.rs
+++ schmivitz/src/vole.rs
@@ -21,6 +21,7 @@ pub(crate) mod consistency_check;
 pub(crate) mod convert_to_vole;
 pub(crate) mod crypto_primitives;
 pub mod functionality;
+pub(crate) mod interface;
 
 /// This defines the behavior needed to create and use non-interactive random VOLEs.
 ///
---
+++ schmivitz/src/vole/interface.rs
@@ -0,0 +1,125 @@
+use super::crypto_primitives::{CHALL1_LENGTH, CHALL3_LENGTH};
+use super::functionality::{decommit, VoleithVerifier};
+use super::{all_but_one_vc::Pdecom, RandomVole};
+use crate::parameters::{REPETITION_PARAM, VOLE_SIZE_PARAM};
+use crate::vole::functionality::{create_voleith_prover, VoleithProver};
+use eyre::{bail, Result};
+use merlin::Transcript;
+use rand::{CryptoRng, RngCore};
+use swanky_field_binary::{F128b, F2};
+
+// This is a first attempt to connect the VOLE part to the circuit traverser.
+
+impl RandomVole for VoleithProver {
+    type Decommitment = Vec<Pdecom>;
+
+    type VoleChallenge = [u8; CHALL1_LENGTH];
+
+    type VoleDecommitmentChallenge = [u8; CHALL3_LENGTH];
+
+    fn create(
+        extended_witness_length: usize,
+        transcript: &mut Transcript,
+        _rng: &mut (impl CryptoRng + RngCore),
+    ) -> (Self, Self::VoleChallenge) {
+        let mut statement_sig = [0u8; 16];
+        transcript.challenge_bytes(b"statement signature", &mut statement_sig);
+        let todo_secret = [42u8];
+        let vole = create_voleith_prover(
+            &statement_sig,
+            &todo_secret,
+            extended_witness_length + REPETITION_PARAM * VOLE_SIZE_PARAM,
+        );
+        let chall = vole.chall1;
+        (vole, chall)
+    }
+
+    fn extract_vole_challenge(
+        _transcript: &mut Transcript,
+        _extended_witness_length: usize,
+    ) -> Self::VoleChallenge {
+        unimplemented!("not totally sure here. func only used on the verifier side")
+    }
+
+    fn count(&self) -> usize {
+        self.u.len()
+    }
+
+    fn extended_witness_length(&self) -> usize {
+        self.count() - REPETITION_PARAM * VOLE_SIZE_PARAM
+    }
+
+    fn witness_mask(&self) -> &[F2] {
+        &self.u[0..self.extended_witness_length()]
+    }
+
+    fn aggregate_commitment_values(&self) -> [F128b; REPETITION_PARAM * VOLE_SIZE_PARAM] {
+        let ell = self.extended_witness_length();
+        self.u[ell..ell + REPETITION_PARAM * VOLE_SIZE_PARAM]
+            .iter()
+            .map(|f2| F128b::from(*f2))
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap()
+    }
+
+    fn aggregate_commitment_masks(&self) -> [F128b; REPETITION_PARAM * VOLE_SIZE_PARAM] {
+        let ell = self.extended_witness_length();
+        self.v[ell..ell + REPETITION_PARAM * VOLE_SIZE_PARAM]
+            .to_vec()
+            .try_into()
+            .unwrap()
+    }
+
+    fn vole_mask(&self, i: usize) -> Result<F128b> {
+        if i < self.extended_witness_length() {
+            Ok(self.v[i])
+        } else {
+            bail!(
+                "vole mask index out of range: should be in [0, {}), but got {}",
+                self.extended_witness_length(),
+                i
+            );
+        }
+    }
+    fn extract_decommitment_challenge(
+        _transcript: &mut Transcript,
+    ) -> Self::VoleDecommitmentChallenge {
+        unimplemented!("not totally sure here")
+    }
+
+    fn decommit(
+        self,
+        transcript: &mut Transcript,
+    ) -> (Self::Decommitment, Self::VoleDecommitmentChallenge) {
+        let decommitment_challenge = Self::extract_decommitment_challenge(transcript);
+        (
+            decommit(&self.decom, decommitment_challenge),
+            decommitment_challenge,
+        )
+    }
+}
+
+// The functions in this implementation are the ones from `InsecureCommitments`
+#[allow(unused)]
+impl VoleithVerifier {
+    pub(crate) fn extended_witness_length(&self) -> usize {
+        self.q.len() - REPETITION_PARAM * VOLE_SIZE_PARAM
+    }
+
+    pub(crate) fn verifier_key_array(&self) -> &F128b {
+        &self.delta
+    }
+
+    pub(crate) fn witness_voles(&self) -> &[F128b] {
+        let count = self.q.len();
+        &self.q[0..count - REPETITION_PARAM * VOLE_SIZE_PARAM]
+    }
+
+    pub(crate) fn mask_voles(&self) -> [F128b; REPETITION_PARAM * VOLE_SIZE_PARAM] {
+        let count = self.q.len();
+        self.q[count - REPETITION_PARAM * VOLE_SIZE_PARAM..count]
+            .try_into()
+            .unwrap()
+    }
+}
