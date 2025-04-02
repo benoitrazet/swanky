81ee386741199d9b311f9d22bb44c1f4b65e0a42
Rawane Issa <rissa@galois.com>
2025-04-01 18:43:14 -0700

    authenticated-bits:draft AuthBitGenerator:generatr

--- crates/authenticated-bits/src/lib.rs
+++ crates/authenticated-bits/src/lib.rs
@@ -1,8 +1,11 @@
+use itertools::Itertools;
 use ocelot::ot::{CorrelatedReceiver, CorrelatedSender};
-use scuttlebut::Malicious;
+use rand::{CryptoRng, Rng};
+use scuttlebutt::{AbstractChannel, Malicious};
 use std::io::{Read, Write};
 use swanky_party::{
-    either::PartyEitherCopy, private::VerifierPrivateCopy, IsParty, Party, Prover, Verifier,
+    either::PartyEither, either::PartyEitherCopy, private::VerifierPrivateCopy, IsParty, Party,
+    Prover, Verifier, WhichParty,
 };
 use vectoreyes::U8x16;
 
@@ -38,7 +41,7 @@ struct VerifierAuthBit {
     /// Local key
     key: U8x16,
 }
-/// A type that represents the Party's part of the authentication bit
+/// A type that represents the Party's part of the authenticated bit
 ///
 /// When `P = Prover`, this value is `ProverAuthBit`
 /// When `P = Verifier`, this value is `VerifierAuthBit`
@@ -46,7 +49,7 @@ struct AuthBit<P: Party> {
     data: PartyEitherCopy<P, ProverAuthBit, VerifierAuthBit>,
 }
 
-/// A struct which contains multiple generated authentication bit
+/// A struct which contains a single authenticated bit
 impl<P: Party> AuthBit<P> {
     /// This outputs the key associated with the AuthBit
     pub fn key(&self, ev: IsParty<P, Verifier>) -> U8x16 {
@@ -82,49 +85,82 @@ impl<P: Party> std::ops::BitXor for AuthBit<P> {
 ///
 /// When `P = Verifier`, this struct also stores the verifier's
 /// global key `delta`.
-struct AuthBitGenerator<
-    P: Party,
-    OTS: CorrelatedSender<Msg = U8x16> + Malicious,
-    OTR: CorrelatedReceiver<Msg = U8x16> + Malicious,
-> {
+struct AuthBitGenerator<P: Party, OTS: CorrelatedSender, OTR: CorrelatedReceiver> {
     /// A vector of authenticated bit.
     data: Vec<AuthBit<P>>,
     /// The verifier's global key.
     delta: VerifierPrivateCopy<P, U8x16>,
-    /// The party's sending OT
-    ots: OTS,
     /// The party's receiving OT
     otr: OTR,
+    /// The party's sender OT
+    ots: OTS,
 }
 
+/// A struct which contains multiple generated authentication bit
 impl<
         P: Party,
         OTS: CorrelatedSender<Msg = U8x16> + Malicious,
         OTR: CorrelatedReceiver<Msg = U8x16> + Malicious,
-    > AuthBitGenerator<P>
+    > AuthBitGenerator<P, OTS, OTR>
 {
     /// Create a new `AuthBitGenerator` based on the type of
     /// the party. In the case of the `Verifier`, store the
     /// `delta` value.
-    pub fn new<RNG>(delta: VerifierPrivateCopy<P, U8x16>, mut channel: C, mut rng: RNG) -> Self
+    pub fn new<C, RNG>(delta: VerifierPrivateCopy<P, U8x16>, mut channel: C, mut rng: RNG) -> Self
     where
         RNG: CryptoRng + Rng,
+        C: AbstractChannel,
     {
         AuthBitGenerator {
             data: vec![],
             delta: delta,
-            ots: OTS::init(&mut channel, &mut rng)?,
-            otr: OTR::init(&mut channel, &mut rng)?,
+            otr: OTR::init(&mut channel, &mut rng).unwrap(),
+            ots: OTS::init(&mut channel, &mut rng).unwrap(),
         }
     }
     // Generate `count` authenticated bits. These are stored in `output`.
-    pub fn generate<C: Read + Write>(
+    pub fn generate<C, RNG>(
         &mut self,
-        channel: C,
+        mut channel: C,
         count: usize,
         output: &mut Vec<AuthBit<P>>,
-    ) -> Result<(), AuthenticationBitError> {
-        todo!()
+        mut rng: RNG,
+    ) -> Result<(), ocelot::Error>
+    where
+        RNG: CryptoRng + Rng,
+        C: AbstractChannel,
+    {
+        match P::WHICH {
+            WhichParty::Prover(ev_pr) => {
+                let bits = vec![rng.gen::<bool>(); count];
+                let macs = self
+                    .otr
+                    .receive_correlated(&mut channel, &bits, &mut rng)
+                    .unwrap();
+                for (i, (bit, mac)) in bits.into_iter().zip(macs).enumerate() {
+                    output[i] = AuthBit {
+                        data: PartyEitherCopy::prover_new(
+                            ev_pr,
+                            ProverAuthBit { bit: bit, mac: mac },
+                        ),
+                    };
+                }
+                Ok(())
+            }
+            WhichParty::Verifier(ev_vr) => {
+                let keys = self.ots.send_correlated(
+                    &mut channel,
+                    &vec![self.delta(ev_vr); count],
+                    &mut rng,
+                )?;
+                for (i, key) in keys.into_iter().enumerate() {
+                    output[i] = AuthBit {
+                        data: PartyEitherCopy::verifier_new(ev_vr, VerifierAuthBit { key: key.0 }),
+                    };
+                }
+                Ok(())
+            }
+        }
     }
     // "Open" a bit.
     // This corresponds to the prover sending $(b, M)$ to the verifier, who checks
