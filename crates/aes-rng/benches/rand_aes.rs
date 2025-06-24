9f1117e0c48712cce54ebecf60ae0797891867f0
Chris Phifer <cphifer@galois.com>
2025-06-24 11:35:58 -0700

    aes-rng/benches/rand_aes: Remove outdated comment.

--- crates/aes-rng/benches/rand_aes.rs
+++ crates/aes-rng/benches/rand_aes.rs
@@ -73,9 +73,6 @@ fn bench_aes_rand_int_126(c: &mut Criterion<Measurement>) {
     );
 }
 
-// NOTE: The CyclesPerByte is inaccurate, since it doesn't make any attempt to serialize execution.
-// It only calls rdstc. Rather than trying to implement the right thing, we'll just increase the
-// sample size and hope for the best.
 criterion_group! {
     name = aesrng;
     config = Criterion::default().with_measurement(new_measurement()).sample_size(4096);
