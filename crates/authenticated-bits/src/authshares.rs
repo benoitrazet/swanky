33d189e6595ee1fe3cd8af2703bb00a71460a661
Alex J Malozemoff <amaloz@galois.com>
2025-06-24 10:26:40 -0700

    crates/authenticated-bits: Clean up `authshares` module documentation.
    Instead of using `lambda = r XOR s`, use the more sensible `x = x_1 XOR x_2`.

--- crates/authenticated-bits/src/authshares.rs
+++ crates/authenticated-bits/src/authshares.rs
@@ -1,10 +1,10 @@
 //! Authenticated shares.
 //!
-//! An authenticated share $`\langle \lambda \rangle = \langle r | s \rangle`$
-//! is a pair of authenticated bits $`[r]_A`$, $`[s]_B`$, where $`[r]_A`$
-//! denotes that $`[r]`$ is an authenticated bit held by Party A, and likewise,
-//! $`[s]_B`$ is an authenticated bit held by Party B. We define $`\lambda = r
-//! \oplus s`$.
+//! An authenticated share $`\langle x \rangle = \langle x_1 | x_2 \rangle`$
+//! is a pair of authenticated bits $`[x_1]_A`$, $`[x_2]_B`$, where $`[x_1]_A`$
+//! denotes that $`[x_1]`$ is an authenticated bit held by Party A, and likewise,
+//! $`[x_2]_B`$ is an authenticated bit held by Party B. We define $`x = x_1
+//! \oplus x_2`$.
 
 use crate::authbits::{AuthBit, AuthBitGenerator};
 use ocelot::ot::{CorrelatedReceiver, CorrelatedSender};
