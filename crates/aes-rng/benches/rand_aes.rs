72a3772c497953bb4f697a59a6c32fbab3434c42
Chris Phifer <cphifer@galois.com>
2025-06-24 11:35:58 -0700

    aes-rng/benches/rand_aes: Remove outdated comment.

--- crates/aes-rng/benches/rand_aes.rs
+++ crates/aes-rng/benches/rand_aes.rs
@@ -1,5 +1,5 @@
 #![allow(clippy::all)]
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::distributions::{Distribution, Uniform};
 use rand_core::RngCore;
 use swanky_aes_rng::{AesRng, UniformIntegersUnderBound};
