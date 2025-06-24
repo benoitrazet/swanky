bd8341ecf61c2e105c11984395d477645f0200a0
Chris Phifer <cphifer@galois.com>
2025-06-23 17:13:56 -0700

    Remove the last vestiges of Scuttlebutt.
    This (finally) removes the actual Scuttlebutt crate (what little was left of
    it); with this, all functionality has been factored into individual crates under
    the `crates/` directory, and nothing in Swanky (libraries, binaries, examples,
    tests, and benchmarks) references `scuttlebutt` any longer.

--- scuttlebutt/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![allow(clippy::all)]
-#![allow(clippy::many_single_char_names)]
-#![deny(missing_docs)]
-// TODO: when https://git.io/JYTnW gets stabilized add the readme as module docs.
-
-//! Scuttlebutt provides many utility functions for cryptographic applications.
