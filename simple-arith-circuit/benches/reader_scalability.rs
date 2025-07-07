a6b1fb441bcb87c1c20662fa6766160cde4fcec1
Marc Rosen <marc@galois.com>
2025-07-07 15:48:58 -0400

    simple-arith-circuit: Generate test circuit in benchmark
    This will allow the benchmark to run in CI without first running an
    external shell script to generate the circuit to test.

--- simple-arith-circuit/benches/reader_scalability.rs
+++ simple-arith-circuit/benches/reader_scalability.rs
@@ -1,17 +1,29 @@
 #![allow(clippy::all)]
 use simple_arith_circuit::Circuit;
-use std::path::PathBuf;
+use std::io::{BufWriter, Write};
 
-// This benchmark depends upon having created a 2.5GB file
-// using the script `make-bench.sh`.
 fn bench_reader_scalability() {
-    let base = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("benches");
-    let ckt = base.join("bf-gigantic.txt");
+    let mut tmp = BufWriter::new(tempfile::NamedTempFile::new().unwrap());
+    let gate_count = 100000000;
+    let wire_count = gate_count + 1;
+    writeln!(tmp, "{gate_count} {wire_count}").unwrap();
+    writeln!(tmp, "1 1").unwrap(); // number-of-input-values number-of-wires-each-input-value...
+    writeln!(tmp, "1 1").unwrap(); // number-of-output-values number-of-wires-each-output-value...
+    writeln!(tmp, "").unwrap();
+    for i in 0..wire_count {
+        writeln!(tmp, "1 1 {i} {} INV", i + 1).unwrap();
+    }
+    tmp.flush().unwrap();
+    let tmp = tmp.into_inner().unwrap();
+    println!("Starting benchmark");
     let start = std::time::Instant::now();
-    Circuit::read_bristol_fashion(&ckt, None).unwrap();
+    Circuit::read_bristol_fashion(tmp.path(), None).unwrap();
     let elapsed = start.elapsed();
     println!("Total: {:#?}", elapsed);
-    println!("Gates per second: {}", 100_000_000 / elapsed.as_secs());
+    println!(
+        "Gates per second: {}",
+        100_000_000.0 / elapsed.as_secs_f64()
+    );
 }
 
 fn main() {
