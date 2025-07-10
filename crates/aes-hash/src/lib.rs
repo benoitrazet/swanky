0589aaa7955ce2250fe23f795db400ee6151e5da
Alex J Malozemoff <amaloz@galois.com>
2025-07-10 16:37:37 -0700

    Add types for correlation-robust hashing
    Before this commit, we had a single type `AesHash` that supports both
    correlation-robust and tweakable circular correlation-robust hashing.
    This is potentially confusing and error prone, as generally you think
    of a hash function as accomplishing _one_ of these capabilities, not
    both.
    
    Thus, this commit introduces `CorrelationRobustHash` and
    `TweakableCircularCorrelationRobustHash` to separate out these concerns
    into two separate types. It also improves the documentation of the
    crate as a whole.

--- crates/aes-hash/src/lib.rs
+++ crates/aes-hash/src/lib.rs
@@ -1,8 +1,39 @@
-#![deny(missing_docs)]
 //! Correlation-robust hashing based on fixed-key AES.
+//!
+//! This module provides objects that enable correlation-robust hashing based on
+//! fixed-key AES. Such objects are useful for efficient OT extension and
+//! circuit garbling, among other uses, and were introduced by Guo, Katz, Wang,
+//! and Yu \[1\].
+//!
+//! A correlation-robust hash function $`H_{\mathsf{cr}}`$ is one that is
+//! resistant to correlations in the hash input. That is, for a fixed $`R`$
+//! (unknown to the adversary), an adversary cannot distinguish between
+//! $`H_{\mathsf{cr}}(x \oplus R)`$ and a random function.
+//!
+//! A circular correlation-robust hash function $`H_{\mathsf{ccr}}`$ is similar,
+//! with the addition of a "circularity" in the use of $`R`$. That is, for a
+//! fixed $`R`$ (again unknown to the adversary), an adversary cannot
+//! distinguish between $`H_{\mathsf{ccr}}(x \oplus R) \oplus b \cdot R`$ and a
+//! random function, where $`b`$ is a bit controllable by the adversary.
+//!
+//! These two notions can be made _tweakable_ in that now the hash function
+//! $`H`$ takes both input $`x`$ and a tweak $`t`$; otherwise, the security
+//! notions are similar.
+//!
+//! In this module we provide a correlation-robust hash function
+//! [`CorrelationRobustHash`] (generally used for semi-honest protocols) and a
+//! tweakable circular correlation-robust hash function
+//! [`TweakableCircularCorrelationRobustHash`] (generally used for malicious
+//! protocols). We also provide a (deprecated) hash function [`AesHash`] that
+//! supports _both_ correlation-robust and tweakable circular
+//! correlation-robust. As this is deprecated, please use one of the direct
+//! instantiations instead.
+//!
+//! \[1\] C. Guo, J. Katz, X. Wang, Y. Yu. "Efficient and Secure Multiparty
+//! Computation from Fixed-Key Block Ciphers." IEEE Security & Privacy. 2020.
+#![deny(missing_docs)]
 
 use std::sync::OnceLock;
-
 use vectoreyes::{
     Aes128EncryptOnly, AesBlockCipher, U8x16,
     array_utils::{ArrayUnrolledExt, ArrayUnrolledOps, UnrollableArraySize},
@@ -11,7 +42,7 @@ use vectoreyes::{
 use swanky_block::Block;
 
 #[derive(Clone)]
-/// AES-based correlation-robust hash function.
+/// AES-based correlation-robust hash function. (Deprecated!)
 ///
 /// This hash function supports the correlation-robust variants given in
 /// <https://eprint.iacr.org/2019/074>.
@@ -21,7 +52,7 @@ pub struct AesHash {
 
 static THE_ONE: OnceLock<AesHash> = OnceLock::new();
 impl AesHash {
-    /// A `AesHash` with a fixed key `b"Aes' 16 byte key"`
+    /// An `AesHash` with a fixed key `b"Aes' 16 byte key"`
     pub fn fixed_key() -> &'static AesHash {
         THE_ONE.get_or_init(|| Self::new(const { U8x16::from_array(*b"Aes' 16 byte key") }))
     }
@@ -66,6 +97,81 @@ impl AesHash {
         );
         let z = self.aes.encrypt_many(t);
         y.array_zip(z).array_map(
+            #[inline(always)]
+            |(a, b)| a ^ b,
+        )
+    }
+}
+
+/// Correlation-robust hash function for 128-bit inputs.
+///
+/// For input $`x`$, the hash function computes $`\pi(x) \oplus x`$,
+/// where $`\pi`$ is AES-128 encryption.
+///
+/// See <https://eprint.iacr.org/2019/074>, §7.2, for details.
+#[derive(Clone, Debug)]
+pub struct CorrelationRobustHash(Aes128EncryptOnly);
+
+static CR_HASH_FIXED_KEY: OnceLock<CorrelationRobustHash> = OnceLock::new();
+impl CorrelationRobustHash {
+    /// Create a [`CorrelationRobustHash`] with a fixed key `b"Aes' 16 byte key"`.
+    pub fn fixed_key() -> &'static Self {
+        CR_HASH_FIXED_KEY
+            .get_or_init(|| Self::new(const { U8x16::from_array(*b"Aes' 16 byte key") }))
+    }
+
+    /// Create a [`CorrelationRobustHash`] using the provided AES-128 key.
+    pub fn new(key: U8x16) -> Self {
+        Self(Aes128EncryptOnly::new_with_key(key))
+    }
+
+    /// Compute the hash function on the given input.
+    #[inline]
+    pub fn hash(&self, input: U8x16) -> U8x16 {
+        self.0.encrypt(input) ^ input
+    }
+}
+
+/// Tweakable circular correlation-robust hash function for 128-bit inputs.
+///
+/// For input $`x`$ and tweak $`t`$, the hash function computes $`\pi(\pi(x)
+/// \oplus t) \oplus \pi(x)`$, where $`\pi`$ is AES-128 encryption.
+///
+/// See <https://eprint.iacr.org/2019/074>, §7.4, for details.
+#[derive(Clone, Debug)]
+pub struct TweakableCircularCorrelationRobustHash(Aes128EncryptOnly);
+
+static TCCR_HASH_FIXED_KEY: OnceLock<TweakableCircularCorrelationRobustHash> = OnceLock::new();
+impl TweakableCircularCorrelationRobustHash {
+    /// A [`TweakableCircularCorrelationRobustHash`] with a fixed key `b"Aes' 16 byte key"`.
+    pub fn fixed_key() -> &'static Self {
+        TCCR_HASH_FIXED_KEY
+            .get_or_init(|| Self::new(const { U8x16::from_array(*b"Aes' 16 byte key") }))
+    }
+
+    /// A [`TweakableCircularCorrelationRobustHash`] using the provided AES-128 key.
+    pub fn new(key: U8x16) -> Self {
+        Self(Aes128EncryptOnly::new_with_key(key))
+    }
+
+    /// Compute the hash function on the given input and tweak.
+    pub fn hash(&self, input: U8x16, tweak: U8x16) -> U8x16 {
+        let permutation = self.0.encrypt(input);
+        let tweaked_permutation = self.0.encrypt(permutation ^ tweak);
+        permutation ^ tweaked_permutation
+    }
+
+    /// Compute the hash function over a batch of inputs.
+    pub fn hash_many<const N: usize>(&self, inputs: [U8x16; N], tweak: U8x16) -> [U8x16; N]
+    where
+        ArrayUnrolledOps: UnrollableArraySize<N>,
+    {
+        let permutations = self.0.encrypt_many(inputs);
+        let tweaked_permutations = self.0.encrypt_many(permutations.array_map(
+            #[inline(always)]
+            |x| x ^ tweak,
+        ));
+        permutations.array_zip(tweaked_permutations).array_map(
             #[inline(always)]
             |(a, b)| a ^ b,
         )
