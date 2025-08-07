c9c588a36d99e64c1d2987699965bf09cece5f67
Marc Rosen <marc@galois.com>
2025-08-07 12:25:14 -0400

    chore: replace nested if let with &&
    In the latest version of Rust, clippy requires this replacement.

--- edge/bit-matrix-transpose/build.rs
+++ edge/bit-matrix-transpose/build.rs
@@ -2,10 +2,10 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=cbits/transpose.c");
     let cargo_target_arch = std::env::var_os("CARGO_CFG_TARGET_ARCH");
-    if let Some(target_arch) = cargo_target_arch {
-        if target_arch != "x86_64" {
-            return;
-        }
+    if let Some(target_arch) = cargo_target_arch
+        && target_arch != "x86_64"
+    {
+        return;
     }
     cc::Build::new()
         .file("cbits/transpose.c")
