897f23928e83bcb2e31677ca7516a17f2ebc5b1a
Rawane Issa <rissa@galois.com>
2025-12-11 09:54:27 -0800

    auth-gc: Add docs + tests to analyzer
    Add documentation and tests for the fancy Analyzer object.

--- edge/authenticated-garbling/src/analyzer.rs
+++ edge/authenticated-garbling/src/analyzer.rs
@@ -1,27 +1,36 @@
 //! Fancy object to compute the number of gates in a binary circuit.
-
+use eyre::{ErrReport, eyre};
 use fancy_garbling::{Fancy, FancyBinary, FancyInput, FancyReveal, HasModulus, errors::FancyError};
-
 #[derive(Clone, Debug)]
+/// An Analyzer Item
+///
+/// A dummy FancyItem which is returned when profiling a [`fancy_garbling::Fancy`] circuit.
+/// The [`AnalyzerItem`] only contains the wire modulus. This is because
+/// [`fancy_garbling::Fancy::Item`] need to implement [`HasModulus`].
 pub struct AnalyzerItem {
     modulus: u16,
 }
 impl HasModulus for AnalyzerItem {
+    /// Returns the modulus of the current wire.
+    /// Since [`Analyzer`] is only defined for binary
+    /// circuits, for now, the modulus should always be 2.
     fn modulus(&self) -> u16 {
         self.modulus
     }
 }
 
-/// Errors thrown by the Fancy computation.
 #[derive(Debug)]
+/// Errors from the [`Analyzer`] fancy object
+///
+/// This error wraps any underlying error thrown by
+/// [`fancy_garbling::Fancy`] with eyre.
 pub enum AnalyzerError {
-    /// Error from Fancy library.
-    Underlying(FancyError),
+    Underlying(ErrReport),
 }
 
 impl From<FancyError> for AnalyzerError {
     fn from(e: FancyError) -> Self {
-        AnalyzerError::Underlying(e)
+        AnalyzerError::Underlying(eyre!(e))
     }
 }
 
@@ -33,8 +42,13 @@ impl std::fmt::Display for AnalyzerError {
     }
 }
 
-/// Fancy Object which computes information about the circuit
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+/// A [`fancy_garbling::Fancy`] Object which counts gates in a binary circuit.
+///
+/// Specifically, [`Analyzer`] stores the number of inputs,
+/// ands, xors, negations and constants in the circuits. This
+/// information is especially useful for pre-processing authenticated
+/// garbling circuits.
 pub struct Analyzer {
     ninputs: usize,
     nands: usize,
@@ -60,7 +74,8 @@ impl std::fmt::Display for Analyzer {
 }
 
 impl Analyzer {
-    /// Create a new Analyzer
+    /// Create a new fresh [`Analyzer`] and sets all the gate
+    /// counts to 0.
     pub fn new() -> Analyzer {
         Analyzer {
             ninputs: 0,
@@ -70,12 +85,22 @@ impl Analyzer {
             nconstants: 0,
         }
     }
+    /// Return the number of AND gates in the circuit
+    pub fn nands(&self) -> usize {
+        self.nands
+    }
+    /// Return the number of input wires of the circuit
+    pub fn ninputs(&self) -> usize {
+        self.ninputs
+    }
 }
 
 impl FancyInput for Analyzer {
     type Item = AnalyzerItem;
     type Error = AnalyzerError;
-
+    /// Receive the other party's garbled inputs. Because [`Analyzer`] does not actually
+    /// perform the circuit computation and instead just analyzes the structure of the circuit,
+    /// this function does not incur any communication costs, i.e. no OT is invoked here.
     fn receive_many(&mut self, moduli: &[u16]) -> Result<Vec<Self::Item>, Self::Error> {
         self.ninputs += moduli.len();
         Ok(moduli
@@ -83,7 +108,9 @@ impl FancyInput for Analyzer {
             .map(|q| AnalyzerItem { modulus: *q })
             .collect())
     }
-
+    /// Encode the current party's garbled inputs. Because [`Analyzer`] does not actually
+    /// perform the circuit computation and instead just analyzes the structure of the circuit,
+    /// this function does not incur any communication costs, i.e. no OT is invoked here.
     fn encode_many(
         &mut self,
         _values: &[u16],
@@ -93,7 +120,24 @@ impl FancyInput for Analyzer {
     }
 }
 
+/// [`Analyzer`] is only implemented for now for [`fancy_garbling::FancyBinary`]
+///
+/// Each [`fancy_garbling::FancyBinary`] operation is translated to the number of
+/// underlying "basic" gates which constitute it. For example, [`fancy_garbling::FancyBinary::xor`]
+/// is a single XOR gate, while [`fancy_garbling::FancyBinary::adder`] is made up of
+/// 5 XOR gates and 2 AND gates. These numbers are directly taken from [`fancy_garbling::FancyBinary`].
+///
+/// Note 1: The 3 "basic" gates are XOR, AND, NEG, while the 2 "basic" input gates are the
+/// input wires and the constant gates.
+///
+/// Note 2: The more complex functions found in [`fancy_garbling::BinaryGadgets`] all
+/// use these basic building blocks and that [`Analyzer`] will hence provide these counts for them
+/// as well.
+///
+/// Note 3: Again recall that [`Analyzer`] will not perform the actual circuit computation and will
+/// instead only count the number of "basic" gates and return the dummy item [`AnalyzerItem`]
 impl FancyBinary for Analyzer {
+    ///
     fn xor(&mut self, x: &Self::Item, _y: &Self::Item) -> Result<Self::Item, Self::Error> {
         self.nxors += 1;
         Ok(AnalyzerItem { modulus: x.modulus })
@@ -171,6 +215,7 @@ impl Fancy for Analyzer {
     type Error = AnalyzerError;
 
     fn constant(&mut self, _val: u16, q: u16) -> Result<Self::Item, Self::Error> {
+        self.nconstants += 1;
         Ok(AnalyzerItem { modulus: q })
     }
 
@@ -190,7 +235,7 @@ mod tests {
     use super::*;
     use fancy_garbling::BinaryGadgets;
     #[test]
-    fn test_analyzer() {
+    fn test_single_and() {
         let nbits = 64;
         let x = 0;
         let y = 0;
@@ -206,6 +251,77 @@ mod tests {
             nxors: 0,
             nnegs: 0,
             nconstants: 0,
+        };
+        assert_eq!(analyzer_test, analyzer_correct);
+    }
+    #[test]
+    fn test_binady_addition() {
+        let nbits = 64;
+        let x = 0;
+        let y = 0;
+        let mut analyzer_test: Analyzer = Analyzer::new();
+        {
+            let x = analyzer_test.bin_encode(x, nbits).unwrap();
+            let y = analyzer_test.bin_encode(y, nbits).unwrap();
+            // bin_addition is equivalent to an "adder" per wire
+            let _c = analyzer_test.bin_addition(&x, &y);
+        }
+
+        let analyzer_correct = Analyzer {
+            ninputs: 128,
+            // number of ands in an adder
+            nands: 64 * 2,
+            // number of xors in an adder
+            nxors: 64 * 5,
+            nnegs: 0,
+            nconstants: 0,
+        };
+        assert_eq!(analyzer_test, analyzer_correct);
+    }
+    #[test]
+    fn test_binary_multiplication() {
+        let nbits = 64;
+        let x = 0;
+        let y = 0;
+        let mut analyzer_test: Analyzer = Analyzer::new();
+        {
+            let x = analyzer_test.bin_encode(x, nbits).unwrap();
+            let y = analyzer_test.bin_encode(y, nbits).unwrap();
+            // bin_addition is equivalent to an "adder" per wire
+            let _c = analyzer_test.bin_mul(&x, &y);
+        }
+
+        // In binary multiplication there are :
+        // - 64 * 64 explicit ANDs, i.e. pairwise ANDS between parties input wires.
+        // - Sum(65 to 128) binary additions. Recall that in multiplication the input
+        //   size grows by 1 bit each round until each 2 times the size of the initial
+        //   input (i.e. 64 * 2)
+        let analyzer_correct = Analyzer {
+            ninputs: 128,
+            nands: (65..128).sum::<usize>() * 2 + 64 * 64,
+            nxors: (65..128).sum::<usize>() * 5,
+            nnegs: 0,
+            nconstants: 64,
+        };
+        assert_eq!(analyzer_test, analyzer_correct);
+    }
+    #[test]
+    fn test_binady_twos_complement() {
+        let nbits = 64;
+        let x = 0;
+        let mut analyzer_test: Analyzer = Analyzer::new();
+        {
+            let x = analyzer_test.bin_encode(x, nbits).unwrap();
+            // bin_addition is equivalent to an "adder" per wire
+            let _c = analyzer_test.bin_twos_complement(&x);
+        }
+
+        let analyzer_correct = Analyzer {
+            ninputs: 64,
+            nands: 63 * 2,
+            nxors: 63 * 5 + 3,
+            nnegs: 64,
+            nconstants: 64,
         };
         assert_eq!(analyzer_test, analyzer_correct);
     }
