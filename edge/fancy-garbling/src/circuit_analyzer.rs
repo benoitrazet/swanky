d27206aebf72884a3c2b7b74e008d2d9cba16975
Rawane Issa <rissa@galois.com>
2026-01-19 11:19:36 -0800

    auth-gc: Remove unnecessary analyzer impls
    This commit mainly removes unnecessary implements inside analyzer since we can build any binary circuit using ANDs, XORs and NEGATE and fix bugs that arise from that change in the tests.
    
    This commit also removes deprecated docs.

--- edge/fancy-garbling/src/circuit_analyzer.rs
+++ edge/fancy-garbling/src/circuit_analyzer.rs
@@ -1,4 +1,5 @@
-//! Fancy object to compute the multiplicative depth of a computation.
+//! Fancy object to profile a fancy circuit and compute stats such as the multiplicative depth
+//! or the number of boolean and arithmetic gates in a circuit.
 
 use crate::{
     FancyArithmetic, FancyBinary,
@@ -55,8 +56,6 @@ impl std::fmt::Display for AnalyzerError {
 }
 impl Error for AnalyzerError {}
 
-/// Fancy Object which computes information about the circuit of interest to FHE.
-///
 /// A [`Fancy`] object which counts gates in a binary circuit.
 ///
 /// Specifically, [`CircuitAnalyzer`] stores the number of inputs,
@@ -193,106 +192,6 @@ impl FancyBinary for CircuitAnalyzer {
             depth: x.depth,
         })
     }
-    fn adder(
-        &mut self,
-        x: &Self::Item,
-        y: &Self::Item,
-        carry_in: Option<&Self::Item>,
-    ) -> Result<(Self::Item, Self::Item), Self::Error> {
-        // Fancy implements adders with 5 XORs and 2 ANDs
-        self.nands += 2;
-        self.nxors += 5;
-        // Fancy's AND gate calls the underlying arithmetic multiplication
-        self.nmuls += 2;
-        // Fancy's XOR gate calls the underlying arithmetic addition
-        self.nadds += 5;
-        // We need to take the carry_in's depth into account as it may come from elsewhere in the circuit
-        let carry_depth = { if let Some(v) = carry_in { v.depth } else { 0 } };
-        Ok((
-            AnalyzerItem {
-                modulus: x.modulus,
-                // An adder is comprised of two ANDS, which increase the multiplication depth
-                // by 2. We first however need to check which of the inputs has highest depths.
-                depth: max(max(x.depth, y.depth), carry_depth) + 2,
-            },
-            AnalyzerItem {
-                modulus: x.modulus,
-                depth: max(max(x.depth, y.depth), carry_depth) + 2,
-            },
-        ))
-    }
-    fn and_many(&mut self, args: &[Self::Item]) -> Result<Self::Item, Self::Error> {
-        self.nands += args.len();
-        // Fancy's AND gate calls the underlying arithmetic multiplication
-        self.nmuls += args.len();
-        Ok(AnalyzerItem {
-            modulus: args[0].modulus,
-            // The gates comprising this function are all sequential
-            depth: args.iter().fold(args[0].depth, |acc, x| max(acc, x.depth)) + args.len(),
-        })
-    }
-    fn or_many(&mut self, args: &[Self::Item]) -> Result<Self::Item, Self::Error> {
-        // Fancy implements OR with 3 NEGs and 1 AND.
-        self.nands += args.len();
-        self.nnegs += 3 * args.len();
-        // Recall that a negation is made of 1 constant input and 1 XOR
-        self.nconstants += args.len();
-        self.nxors += args.len();
-        // Fancy's AND gate calls the underlying arithmetic multiplication
-        self.nmuls += args.len();
-        // Fancy's XOR gate calls the underlying arithmetic addition
-        self.nadds += args.len();
-        Ok(AnalyzerItem {
-            modulus: args[0].modulus,
-            depth: args.iter().fold(args[0].depth, |acc, x| max(acc, x.depth)) + args.len(),
-        })
-    }
-    fn xor_many(&mut self, args: &[Self::Item]) -> Result<Self::Item, Self::Error> {
-        self.nxors += args.len();
-        // Fancy's XOR gate calls the underlying arithmetic addition
-        self.nadds += args.len();
-        Ok(AnalyzerItem {
-            modulus: args[0].modulus,
-            depth: args.iter().fold(args[0].depth, |acc, x| max(acc, x.depth)),
-        })
-    }
-    fn mux_constant_bits(
-        &mut self,
-        x: &Self::Item,
-        _b1: bool,
-        _b2: bool,
-    ) -> Result<Self::Item, Self::Error> {
-        self.nnegs += 1;
-        self.nconstants += 2;
-
-        // Fancy implements negation with one constant gate and one XOR
-        self.nconstants += 1;
-        self.nxors += 1;
-
-        // Fancy's XOR gate calls the underlying arithmetic addition
-        self.nadds += 1;
-        Ok(AnalyzerItem {
-            modulus: x.modulus,
-            depth: x.depth,
-        })
-    }
-    fn mux(
-        &mut self,
-        b: &Self::Item,
-        x: &Self::Item,
-        y: &Self::Item,
-    ) -> Result<Self::Item, Self::Error> {
-        self.nands += 1;
-        self.nxors += 2;
-        // Fancy's AND gate calls the underlying arithmetic multiplication
-        self.nmuls += 2;
-        // Fancy's XOR gate calls the underlying arithmetic addition
-        self.nadds += 2;
-        Ok(AnalyzerItem {
-            modulus: x.modulus,
-            depth: max(max(x.depth, y.depth), b.depth) + 1,
-        })
-    }
 }
 
 impl FancyArithmetic for CircuitAnalyzer {
@@ -398,8 +297,12 @@ mod tests {
         }
 
         assert_eq!(analyzer_test.ninputs, 128);
-        assert_eq!(analyzer_test.nands, 64 * 2);
-        assert_eq!(analyzer_test.nxors, 64 * 5);
+        assert_eq!(analyzer_test.nands, 64);
+        // There are (nbits -1) adders invoked with a carry
+        // and the very first one without. The adders with
+        // carry have 3 extra XORs, check fancy_garbling::adder
+        // and fancy_garbling::binary_addition for more info
+        assert_eq!(analyzer_test.nxors, 64 * 4 - 3);
         assert_eq!(analyzer_test.nconstants, 0);
         assert_eq!(analyzer_test.nnegs, 0);
     }
@@ -422,8 +325,11 @@ mod tests {
         //   size grows by 1 bit each round until each 2 times the size of the initial
         //   input (i.e. 64 * 2)
         assert_eq!(analyzer_test.ninputs, 128);
-        assert_eq!(analyzer_test.nands, (65..128).sum::<usize>() * 2 + 64 * 64);
-        assert_eq!(analyzer_test.nxors, (65..128).sum::<usize>() * 5);
+        assert_eq!(analyzer_test.nands, (65..128).sum::<usize>() + 64 * 64);
+        assert_eq!(
+            analyzer_test.nxors,
+            (65..128).sum::<usize>() * 4 - (3 * (128 - 65))
+        );
         assert_eq!(analyzer_test.nconstants, 64);
         assert_eq!(analyzer_test.nnegs, 0);
     }
@@ -439,8 +345,8 @@ mod tests {
         }
 
         assert_eq!(analyzer_test.ninputs, 64);
-        assert_eq!(analyzer_test.nands, 63 * 2);
-        assert_eq!(analyzer_test.nxors, 63 * 5 + 3 + 64);
+        assert_eq!(analyzer_test.nands, 63);
+        assert_eq!(analyzer_test.nxors, 63 * 4 - 3 + 64 + 2);
         assert_eq!(analyzer_test.nconstants, 64 + 64);
         assert_eq!(analyzer_test.nnegs, 64);
     }
