0557bc1ace86fa24d6c668add3964b9d224b3404
Chris Phifer <cphifer@galois.com>
2026-01-23 04:36:28 +0000

    etc/nix/musl-headers.nix: Update to musl 1.2.5.
    See https://blog.rust-lang.org/2025/12/05/Updating-musl-1.2.5/

--- etc/nix/musl-headers.nix
+++ etc/nix/musl-headers.nix
@@ -1,14 +1,14 @@
 { target }:
 with import ./pkgs.nix { };
 let
-  version = "1.2.3";
+  version = "1.2.5";
   swankyLlvm = import ./llvm.nix;
 in
 stdenv.mkDerivation {
   name = "musl-sysroot";
   src = fetchurl {
     url = "https://musl.libc.org/releases/musl-${version}.tar.gz";
-    hash = "sha256-fVsLYGJSHkYn4JnkydyCSNMqMChelZt+7Kp4DPjP1KQ=";
+    hash = "sha256-qaEYu+hNh2TaDqDSizqz+uhHf8fkCF2QECuFlvx8deQ=";
   };
   configurePhase = ''
     export CC="${swankyLlvm.clang-unwrapped}/bin/clang"
