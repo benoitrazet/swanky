d1317640e4c59974d1c40442c457d1537d771e83
Alex J Malozemoff <amaloz@galois.com>
2026-02-11 13:30:58 -0800

    `garbled-nn`: Apply `black` formatting

--- edge/garbled-nn/neural_nets/reweight.py
+++ edge/garbled-nn/neural_nets/reweight.py
@@ -1,28 +1,23 @@
-
-import models as models
-import data as data
-import save as s
-from sklearn.metrics import classification_report
-from keras.models import load_model
 import argparse
 
+import data as data
+import models as models
+import save as s
+from discretize import custom_scale, scale_round
+from keras.models import load_model
+from sklearn.metrics import classification_report
 from train import evaluate
-from discretize import scale_round, custom_scale
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="To be announced...")
 
-
-
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(description='To be announced...')
-
-    parser.add_argument('--h5model')
-    parser.add_argument("--batchsize",default=32,type=int)
-    parser.add_argument("--scale",default=100,type=int)
-    parser.add_argument("--tolerance",default=3,type=int)
-    parser.add_argument("--savedir",default="",type=str)
-    parser.add_argument("--save",action="store_true")
-    parser.add_argument("--custom",action="store_true")
-
+    parser.add_argument("--h5model")
+    parser.add_argument("--batchsize", default=32, type=int)
+    parser.add_argument("--scale", default=100, type=int)
+    parser.add_argument("--tolerance", default=3, type=int)
+    parser.add_argument("--savedir", default="", type=str)
+    parser.add_argument("--save", action="store_true")
+    parser.add_argument("--custom", action="store_true")
 
     args = parser.parse_args()
 
@@ -47,15 +42,15 @@ if __name__=="__main__":
     y_test = dataset["y_test"]
 
     print("\nINFO: Evaluating original model")
-    accuracy = evaluate(model,x_test,y_test,batch_size)
+    accuracy = evaluate(model, x_test, y_test, batch_size)
 
     weights = model.get_weights()
     print(weights[-1])
 
     if args.custom:
-        discrete_weights = custom_scale(weights,tolerance)
+        discrete_weights = custom_scale(weights, tolerance)
     else:
-        discrete_weights = scale_round(weights,scale)
+        discrete_weights = scale_round(weights, scale)
 
     model.set_weights(discrete_weights)
 
@@ -63,10 +58,9 @@ if __name__=="__main__":
     print(model.get_weights()[-1])
 
     print("\nINFO: Evaluating discretized model")
-    evaluate(model,x_test,y_test,batch_size)
+    evaluate(model, x_test, y_test, batch_size)
 
     if args.save:
         print("\nINFO: Saving trained model files to {}".format(savedir))
-        s.save_model(model,savedir)
-        s.save_weights(discrete_weights,savedir)
-
+        s.save_model(model, savedir)
+        s.save_weights(discrete_weights, savedir)
--- edge/garbled-nn/neural_nets/train.py
+++ edge/garbled-nn/neural_nets/train.py
@@ -1,44 +1,47 @@
-
-import models as models
-import data as data
-import save as s
-import time
-from sklearn.metrics import classification_report
 import argparse
+import time
 
+import data as data
+import models as models
+import save as s
 from discretize import scale_round
+from sklearn.metrics import classification_report
 
 
-def evaluate(model,x_test,y_test,batch_size):
+def evaluate(model, x_test, y_test, batch_size):
     try:
         print()
-        print(classification_report(y_test.argmax(axis=1),predictions.argmax(axis=1)))
+        print(classification_report(y_test.argmax(axis=1), predictions.argmax(axis=1)))
     except:
         print("WARNING: unable to compute classification report")
 
     score = model.evaluate(x_test, y_test, verbose=0)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+    print("Test loss:", score[0])
+    print("Test accuracy:", score[1])
     return score[1]
 
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser(description='To be announced...')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="To be announced...")
 
-    parser.add_argument('--model',
-                        choices=["SecureML_MLP_MNIST",
-                                 "CryptoNets_CNN_MNIST",
-                                 "MiniONN_CNN_MNIST",
-                                 "MiniONN_CNN_CIFAR",
-                                 "DeepSecure_CNN_MNIST",
-                                 "test_MLP_MNIST"])
-    parser.add_argument("--batchsize",default=32,type=int)
-    parser.add_argument("--epochs",default=100,type=int)
-    parser.add_argument("--savedir",type=str)
-    parser.add_argument("--alternate",action="store_true")
+    parser.add_argument(
+        "--model",
+        choices=[
+            "SecureML_MLP_MNIST",
+            "CryptoNets_CNN_MNIST",
+            "MiniONN_CNN_MNIST",
+            "MiniONN_CNN_CIFAR",
+            "DeepSecure_CNN_MNIST",
+            "test_MLP_MNIST",
+        ],
+    )
+    parser.add_argument("--batchsize", default=32, type=int)
+    parser.add_argument("--epochs", default=100, type=int)
+    parser.add_argument("--savedir", type=str)
+    parser.add_argument("--alternate", action="store_true")
 
     args = parser.parse_args()
-    print("alternate set to: ",args.alternate)
+    print("alternate set to: ", args.alternate)
     batch_size = args.batchsize
     epochs = args.epochs
     savedir = args.savedir
@@ -56,8 +59,10 @@ if __name__=="__main__":
         scale = 10
 
     elif args.model == "CryptoNets_CNN_MNIST":
-        model = models.CryptoNets_CNN_MNIST(pooling="max",alternate=args.alternate)
-        discrete_model = models.CryptoNets_CNN_MNIST(pooling="max",alternate=args.alternate)
+        model = models.CryptoNets_CNN_MNIST(pooling="max", alternate=args.alternate)
+        discrete_model = models.CryptoNets_CNN_MNIST(
+            pooling="max", alternate=args.alternate
+        )
 
         scale = 100
 
@@ -68,8 +73,10 @@ if __name__=="__main__":
         scale = 100
 
     elif args.model == "MiniONN_CNN_CIFAR":
-        model = models.MiniONN_CNN_CIFAR(pooling="max",alternate=args.alternate)
-        discrete_model = models.MiniONN_CNN_CIFAR(pooling="max",alternate=args.alternate)
+        model = models.MiniONN_CNN_CIFAR(pooling="max", alternate=args.alternate)
+        discrete_model = models.MiniONN_CNN_CIFAR(
+            pooling="max", alternate=args.alternate
+        )
 
         scale = 100
 
@@ -92,28 +99,30 @@ if __name__=="__main__":
     y_train = dataset["y_train"]
     y_test = dataset["y_test"]
 
- 
-    model.fit(x_train, y_train, validation_data=(x_test,y_test),
-              epochs=epochs,batch_size=batch_size)
-
+    model.fit(
+        x_train,
+        y_train,
+        validation_data=(x_test, y_test),
+        epochs=epochs,
+        batch_size=batch_size,
+    )
 
     model.save(savedir + args.model + "_" + str(int(time.time())) + ".h5")
 
     print("\nINFO: Evaluating model")
-    evaluate(model,x_test,y_test,batch_size)
+    evaluate(model, x_test, y_test, batch_size)
 
     weights = model.get_weights()
 
-    discrete_weights = scale_round(weights,scale) # TODO: make this better
+    discrete_weights = scale_round(weights, scale)  # TODO: make this better
     discrete_model.set_weights(discrete_weights)
 
     print("\nINFO: Evaluating discretized model")
-    evaluate(discrete_model,x_test,y_test,batch_size)
+    evaluate(discrete_model, x_test, y_test, batch_size)
 
     print("\nINFO: Saving trained model files to {}".format(savedir))
 
-
-    s.save_model(discrete_model,savedir)
-    s.save_weights(discrete_weights,savedir)
-    s.save_x_test(x_test.astype(int),savedir)
-    s.save_y_test(y_test,savedir)
+    s.save_model(discrete_model, savedir)
+    s.save_weights(discrete_weights, savedir)
+    s.save_x_test(x_test.astype(int), savedir)
+    s.save_y_test(y_test, savedir)
