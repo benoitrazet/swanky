5bf71ac744e76a52177536900fc6f7b33f42ce45
James Parker <james@galois.com>
2026-03-04 15:40:04 -0500

    Move examples into documentation

--- edge/sieve-ir-api/src/lib.rs
+++ edge/sieve-ir-api/src/lib.rs
@@ -2,6 +2,70 @@
 //! [SIEVE IR](https://github.com/sieve-zk/ir). This provides a programmatic way to write circuits,
 //! which should be faster than parsing a circuit dynamically.
 //!
+//! Here's a simple example that demonstrates how to write a boolean circuit programmatically.
+//! ```
+//! use swanky_field_binary::F2;
+//! use swanky_sieve_ir_api::*;
+//!
+//! fn example1<B>(backend: &mut B) -> CircuitResult<()>
+//! where
+//!     B: FieldBackend<F2>,
+//! {
+//!     let v0 = backend.input_private()?;
+//!     let v1 = backend.mul(&v0, &v0)?;
+//!     let v2 = backend.add(&v1, &v1)?;
+//!
+//!     backend.assert_zero(&v2)?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! Here's another example that defines a circuit with multiple fields.
+//! ```
+//! use swanky_field_binary::F2;
+//! use swanky_field_ff_primes::F128p;
+//! use swanky_sieve_ir_api::*;
+//!
+//! fn helper_circuit<B>(
+//!     backend: &mut B,
+//!     arg1: &B::Wire,
+//!     arg2: &B::Wire,
+//!     arg3: &B::Wire,
+//! ) -> CircuitResult<(B::Wire, B::Wire)>
+//! where
+//!     B: FieldBackend<F2>,
+//! {
+//!     let v1 = backend.add(arg1, arg2)?;
+//!     let v2 = backend.add(arg3, &v1)?;
+//!
+//!     Ok((v1, v2))
+//! }
+//!
+//! fn example2<B>(backend: &mut B) -> CircuitResult<()>
+//! where
+//!     B: FieldBackend<F2>,
+//!     B: FieldBackend<F128p>,
+//! {
+//!     let v0 = <B as FieldBackend<F2>>::input_private(backend)?;
+//!     let v1 = <B as FieldBackend<F2>>::mul(backend, &v0, &v0)?;
+//!     let v2 = <B as FieldBackend<F2>>::add(backend, &v1, &v1)?;
+//!
+//!     <B as FieldBackend<F2>>::assert_zero(backend, &v2)?;
+//!
+//!     let p0 = <B as FieldBackend<F128p>>::input_private(backend)?;
+//!     let p1 = <B as FieldBackend<F128p>>::input_private(backend)?;
+//!     let p2 = <B as FieldBackend<F128p>>::add(backend, &p0, &p1)?;
+//!     <B as FieldBackend<F128p>>::assert_zero(backend, &p2)?;
+//!
+//!     let (v3, v4) = helper_circuit(backend, &v0, &v1, &v2)?;
+//!     <B as FieldBackend<F2>>::assert_zero(backend, &v3)?;
+//!     <B as FieldBackend<F2>>::assert_zero(backend, &v4)?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
 
 #![deny(missing_docs)]
 
