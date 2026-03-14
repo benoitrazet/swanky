6b5f86319315f0a2a0db09c0595f79bbacb46167
Chris Phifer <cphifer@galois.com>
2026-03-14 02:30:15 -0700

    party::private::copy_conversions: Add test suite.
    Exhaustively checks the logical helper functions; runs basic
    conversions for all From<...> implementations over
    PartyPrivate/PartyPrivateCopy (and references to those types).

--- edge/party/src/private/copy_conversions.rs
+++ edge/party/src/private/copy_conversions.rs
@@ -105,3 +105,90 @@ impl<'a, PrivateTo: GenericParty<PartySystem = P::PartySystem>, P: GenericParty,
 }
 
 // TODO: we can do this for more containers.
+
+#[cfg(test)]
+#[allow(dead_code)]
+mod tests {
+    use super::*;
+    party_system! {
+        mod ps {
+            PartyA,
+            PartyB,
+        }
+    }
+    use ps::{PartyA, PartyB};
+
+    #[test]
+    fn copy_ev_exhaustive() {
+        let w = copy_ev::<PartyA, PartyA, _>();
+        let pp: PartyPrivate<PartyA, PartyA, _> = PartyPrivate::default();
+        let pp_copy: PartyPrivateCopy<PartyA, PartyA, _> = PartyPrivateCopy(w.cast(pp.0));
+        assert_eq!(pp.0, pp_copy.0);
+
+        let pp: PartyPrivate<PartyB, PartyB, _> = PartyPrivate::new_with(|| 17);
+        let pp_copy: PartyPrivateCopy<PartyB, PartyB, _> = PartyPrivateCopy(w.cast(pp.0));
+        assert_eq!(pp.0, pp_copy.0);
+
+        let w = copy_ev::<PartyB, PartyA, i32>();
+        let pp: PartyPrivate<PartyB, PartyA, _> = PartyPrivate::new(17);
+        let pp_copy: PartyPrivateCopy<PartyB, PartyA, i32> = PartyPrivateCopy(w.cast(pp.0));
+        assert_eq!(pp.0, pp_copy.0);
+
+        let pp: PartyPrivate<PartyA, PartyB, _> = PartyPrivate::new_with(|| 17);
+        let pp_copy: PartyPrivateCopy<PartyA, PartyB, i32> = PartyPrivateCopy(w.cast(pp.0));
+        assert_eq!(pp.0, pp_copy.0);
+    }
+
+    #[test]
+    #[allow(clippy::clone_on_copy)]
+    fn private_to_private_copy() {
+        let pp: PartyPrivate<PartyA, PartyA, _> = PartyPrivate::new(17);
+        let pp_copy = PartyPrivateCopy::from(pp.clone());
+        assert_eq!(
+            pp.unwrap_or_else(|| unreachable!()),
+            pp_copy.clone().unwrap_or_else(|| unreachable!())
+        );
+    }
+
+    #[test]
+    fn private_to_private_copy_refs() {
+        let pp_ref: &PartyPrivate<PartyA, PartyA, _> = &PartyPrivate::new(17);
+        let pp_copy_ref = <&PartyPrivateCopy<_, _, _>>::from(pp_ref);
+        assert_eq!(
+            pp_ref.clone().unwrap_or_else(|| unreachable!()),
+            pp_copy_ref.unwrap_or_else(|| unreachable!())
+        );
+    }
+
+    #[test]
+    fn private_to_private_copy_mut_refs() {
+        let pp_mut_ref: &mut PartyPrivate<PartyA, PartyA, _> = &mut PartyPrivate::new(17);
+        let pp_copy_mut_ref = <&mut PartyPrivateCopy<_, _, _>>::from(pp_mut_ref);
+        assert_eq!(pp_copy_mut_ref.unwrap_or_else(|| unreachable!()), 17);
+    }
+
+    #[test]
+    fn private_copy_to_private() {
+        let pp_copy: PartyPrivateCopy<PartyA, PartyB, _> = PartyPrivateCopy::default();
+        let pp = PartyPrivate::from(pp_copy);
+        assert_eq!(pp_copy.unwrap_or_else(|| 17), pp.unwrap_or_else(|| 17),);
+    }
+
+    #[test]
+    fn private_copy_to_private_refs() {
+        let pp_copy_ref: &PartyPrivateCopy<PartyA, PartyB, _> = &PartyPrivateCopy::default();
+        let pp_ref = <&PartyPrivate<_, _, _>>::from(pp_copy_ref);
+        assert_eq!(
+            pp_copy_ref.unwrap_or_else(|| 17),
+            pp_ref.clone().unwrap_or_else(|| 17)
+        );
+    }
+
+    #[test]
+    fn private_copy_to_private_mut_refs() {
+        let pp_copy_mut_ref: &mut PartyPrivateCopy<PartyA, PartyB, _> =
+            &mut PartyPrivateCopy::default();
+        let pp_mut_ref = <&mut PartyPrivate<_, _, _>>::from(pp_copy_mut_ref);
+        assert_eq!(pp_mut_ref.clone().unwrap_or_else(|| 17), 17);
+    }
+}
