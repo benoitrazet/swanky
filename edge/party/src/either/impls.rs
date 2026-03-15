3e4062166999d81f54ba4453d26c3c6d664c6c26
Chris Phifer <cphifer@galois.com>
2026-03-14 18:17:04 -0700

    party::either::impls: Add test suite.
    Basic coverage of the iterator methods next() and size_hint() (the
    latter because our own implementation is specified to use the
    underlying iterator size_hint()).

--- edge/party/src/either/impls.rs
+++ edge/party/src/either/impls.rs
@@ -36,3 +36,59 @@ impl<P: GenericParty, A: ExactSizeIterator, B: ExactSizeIterator> ExactSizeItera
     for PartyEither<P, A, B>
 {
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ty_eq::Witness;
+
+    use super::*;
+
+    party_system! {
+        mod ps {
+            PartyA,
+            PartyB,
+        }
+    }
+    use ps::{PartyA, PartyB};
+
+    #[test]
+    fn iter_next() {
+        let iter_a: std::iter::Once<_> = std::iter::once(1);
+        let iter_b: std::iter::Once<_> = std::iter::once("a");
+
+        let mut pe_a: PartyEither<PartyA, std::iter::Once<_>, std::iter::Once<_>> =
+            PartyEither::new(Witness::EQUAL_TYPES, iter_a);
+        let mut pe_b: PartyEither<PartyB, std::iter::Once<_>, std::iter::Once<_>> =
+            PartyEither::new(Witness::EQUAL_TYPES, iter_b);
+
+        assert_eq!(
+            pe_a.next(),
+            Some(PartyEither::<PartyA, i32, &str>::new(
+                Witness::EQUAL_TYPES,
+                1
+            ))
+        );
+
+        assert_eq!(
+            pe_b.next(),
+            Some(PartyEither::<PartyB, i32, &str>::new(
+                Witness::EQUAL_TYPES,
+                "a"
+            ))
+        );
+    }
+
+    #[test]
+    fn iter_size_hint() {
+        let iter_a = std::iter::once(1);
+        let iter_b = std::iter::once("a");
+
+        let pe_a: PartyEither<PartyA, std::iter::Once<_>, std::iter::Once<&str>> =
+            PartyEither::new(Witness::EQUAL_TYPES, iter_a);
+        let pe_b: PartyEither<PartyB, std::iter::Once<i32>, std::iter::Once<_>> =
+            PartyEither::new(Witness::EQUAL_TYPES, iter_b);
+
+        assert_eq!(pe_a.size_hint(), (1, Some(1)));
+        assert_eq!(pe_b.size_hint(), (1, Some(1)));
+    }
+}
