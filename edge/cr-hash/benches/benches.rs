83722b61caee600daf5f3137513ac7e8337ae57f
Alex J Malozemoff <amaloz@galois.com>
2026-03-30 14:10:19 -0700

    `swanky-cr-hash`: Rename benchmarks

--- edge/cr-hash/benches/benches.rs
+++ edge/cr-hash/benches/benches.rs
@@ -26,8 +26,8 @@ fn bench_tccr_hash(c: &mut Criterion) {
 }
 
 criterion_group! {
-    name = aeshash;
+    name = benches;
     config = Criterion::default();
     targets = bench_cr_hash, bench_tccr_hash
 }
-criterion_main!(aeshash);
+criterion_main!(benches);
