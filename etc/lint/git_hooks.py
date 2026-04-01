46e2af822d44d1ca9f944ddf6e9f5e7a72a94352
Chris Phifer <cphifer@galois.com>
2026-03-31 19:56:33 -0700

    etc/lint/git_hooks.py: Compute actual core crates from Cargo.toml.
    This commit computes a value actual_core_crates, which is a set of
    strings (names of Swanky crates) that belong to Swanky's core.
    
    Computes this set via comprehension: Filter all of the workspace
    dependencies to those that are (1) members of the workspace (implicit
    via use of `path`) and (2) members of core/.
    
    We use the dependencies instead of the members, because here the names
    of crates _include_ the `swanky-` prefix (as is the case in the
    pre-push hook this lint will eventually read to compare to this set).
    This is a matter of convenience more than anything.

--- etc/lint/git_hooks.py
+++ etc/lint/git_hooks.py
@@ -1,3 +1,5 @@
+import tomllib
+
 import click
 import rich
 
@@ -12,6 +14,22 @@ def lint_pre_push_hook(ctx: click.Context) -> LintResult:
     Check that all core crates are tested
     """
     any_errors = False
+
+    # Get actual / expected core crates from root manifest
+    cargo_toml_path = ROOT / "Cargo.toml"
+    cargo_toml = cargo_toml_path.read_text()
+
+    try:
+        manifest = tomllib.loads(cargo_toml)
+    except tomllib.TOMLDecodeError:
+        rich.print(f"{ROOT}/Cargo.toml is malformed.")
+        return LintResult.FAILURE
+
+    actual_core_crates = {
+        crate
+        for crate, params in manifest["workspace"]["dependencies"].items()
+        if "path" in params and "core" in params["path"]
+    }
 
     if any_errors:
         return LintResult.FAILURE
