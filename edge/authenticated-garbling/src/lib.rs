5749241cf99d0289c1cd6279192678f05f0dec88
Rawane Issa <rissa@galois.com>
2026-04-15 11:36:39 -0700

    agc: Add some basic tests

--- edge/authenticated-garbling/src/lib.rs
+++ edge/authenticated-garbling/src/lib.rs
@@ -12,6 +12,7 @@ pub mod garbler;
 pub mod preprocesser;
 pub mod wire;
 pub mod unifier;
+mod tests;
 
 /// Mux over bit: if the bit is 0, returns value0, otherwise value1
 pub fn mux(bit: F2, value0: U8x16, value1: U8x16) -> U8x16 {
---
+++ edge/authenticated-garbling/src/tests.rs
@@ -0,0 +1,54 @@
+
+#[cfg(test)]
+mod tests {
+
+    use crate::garbler::{Garbler};
+    use crate::evaluator::{Evaluator};
+
+    use fancy_garbling::{BinaryBundle, BinaryGadgets, Fancy, FancyBinary, };
+    use swanky_channel::Channel;
+    use swanky_rng::SwankyRng;
+
+    fn fancy_sum<F>(
+        f: &mut F,
+        garbler_wires: BinaryBundle<F::Item>,
+        evaluator_wires: BinaryBundle<F::Item>,
+        channel: &mut Channel,
+    ) -> swanky_error::Result<BinaryBundle<F::Item>>
+    where
+        F: Fancy + BinaryGadgets + FancyBinary,
+    {
+        f.bin_addition_no_carry(&garbler_wires, &evaluator_wires, channel)
+    }
+    #[test]
+    fn test_party_construction_passes() {
+        swanky_channel::local::local_channel_pair(
+            |c| {
+                let rng = SwankyRng::new();
+                Garbler::new(rng)
+            },
+            |c| {
+                let rng = SwankyRng::new();
+                Evaluator::new(c, rng)
+            },
+        )
+        .unwrap();
+    }
+        #[test]
+    fn test_party_preprocessing_passes() {
+        let input_size = 400;
+        swanky_channel::local::local_channel_pair(
+            |c| {
+                let rng = SwankyRng::new();
+                let mut gb = Garbler::new(rng).unwrap();
+                gb.preprocess_circuit(&mut fancy_sum, input_size, c)
+            },
+            |c| {
+                let rng = SwankyRng::new();
+               let mut ev =  Evaluator::new(c, rng).unwrap();
+               ev.preprocess_circuit(&mut fancy_sum, input_size, c)
+            },
+        )
+        .unwrap();
+    }
+}
