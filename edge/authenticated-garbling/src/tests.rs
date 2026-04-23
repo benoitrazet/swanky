79be4dfd77fb9d6e2aadf4253c69c589cb21a578
Rawane Issa <rissa@galois.com>
2026-04-23 07:57:26 -0700

    agc: Add tests for encode/receive

--- edge/authenticated-garbling/src/tests.rs
+++ edge/authenticated-garbling/src/tests.rs
@@ -6,6 +6,7 @@ mod tests {
 
     use fancy_garbling::{BinaryBundle, BinaryGadgets, Fancy, FancyBinary};
     use swanky_channel::Channel;
+    use swanky_field_binary::F2;
     use swanky_rng::SwankyRng;
 
     fn fancy_sum<F>(
@@ -46,6 +47,28 @@ mod tests {
                 let rng = SwankyRng::new();
                 let mut ev = Evaluator::new(c, rng).unwrap();
                 ev.preprocess_circuit(&mut fancy_sum, input_size, c)
+            },
+        )
+        .unwrap();
+    }
+    #[test]
+    fn test_party_encoding_receiving_passes() {
+        let gb_input_size = 400;
+        let ev_input_size = 500;
+
+        swanky_channel::local::local_channel_pair(
+            |c| {
+                let rng = SwankyRng::new();
+                let mut gb = Garbler::new(rng, c).unwrap();
+                gb.preprocess_circuit(&mut fancy_sum, gb_input_size ,c).unwrap();
+                gb.encode_many(&vec![0; gb_input_size], &vec![2; gb_input_size], c)
+            },
+            |c| {
+                let rng = SwankyRng::new();
+                let mut ev = Evaluator::new(c, rng).unwrap();
+                ev.preprocess_circuit(&mut fancy_sum, ev_input_size, c).unwrap();
+                ev.set_values(vec![F2::from(0); ev_input_size]);
+                ev.receive_many(&vec![2; ev_input_size], c)
             },
         )
         .unwrap();
