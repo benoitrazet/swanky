2f6fcc43d371354282eec47fcc4353463ea944b5
Alex J Malozemoff <amaloz@galois.com>
2026-06-18 14:48:50 -0700

    `fancy-garbling`: Fix bad docstring

--- edge/fancy-garbling/src/fancy/zk.rs
+++ edge/fancy-garbling/src/fancy/zk.rs
@@ -2,7 +2,8 @@ use crate::FancyEncode;
 use swanky_channel::Channel;
 use swanky_error::Result;
 
-/// Extension trait for [`Fancy`] supporting operations used for zero-knowledge proofs.
+/// Extension trait for [`crate::Fancy`] supporting operations used for
+/// zero-knowledge proofs.
 pub trait FancyZeroKnowledge: FancyEncode {
     /// Assert that `x == 0`.
     fn assert_zero(&mut self, x: &Self::Item, channel: &mut Channel) -> Result<()>;
