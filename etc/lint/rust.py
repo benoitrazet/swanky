ed2d0b65fbd7a5c75b2c0be94ac4bd61d5c584ea
Alex J Malozemoff <amaloz@galois.com>
2026-06-23 12:51:34 -0700

    `fancy-traits`: Update lint to allow name
    Keeping the `fancy-*` naming convention, this change allows
    `fancy-traits` as a "misnamed" crate.

--- etc/lint/rust.py
+++ etc/lint/rust.py
@@ -164,6 +164,7 @@ MISNAMED_CRATES = {
     "inferno",
     "keyed_arena",
     "fancy-garbling",
+    "fancy-traits",
     "mac-n-cheese-compiler",
     "mac-n-cheese-event-log",
     "mac-n-cheese-inspector",
